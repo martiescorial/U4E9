7ff8ef6b07c02dff9e74dff855c0db33bcf6df12
Martina <martinaescorial9@gmail.com>
2021-06-19 08:17:53 +0200

    Add project files.

---
+++ U4E9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace U4E9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            while (i <= 100)
+            {
+                Console.WriteLine(i);
+                i += 2;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
